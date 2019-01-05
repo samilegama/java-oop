623577936efe237a160d9abde5865b2b32db7c1d
samilegama <samilegama@gmail.com>
2019-01-05 12:07:42 -0500

    add variables

---
+++ src/variables/Variables.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		double tuna; //declare
+		tuna = 5.28; //initialize
+		
+		System.out.print("I want ");
+		System.out.print(tuna);
+		System.out.println(" movies");
+		System.out.print("apples");
+		
+	}
+
+}

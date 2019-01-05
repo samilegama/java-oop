0a4eb033485d76b6bdfde5f831201aa5d3bb36e0
samilegama <samilegama@gmail.com>
2019-01-05 12:18:57 -0500

    add Scanner

---
+++ src/getinput/GetUserInput.java
@@ -0,0 +1,12 @@
+package getinput;
+import java.util.Scanner;
+
+public class GetUserInput {
+
+	public static void main(String[] args) {
+		Scanner bucky = new Scanner (System.in);
+		System.out.println(bucky.nextLine());
+
+	}
+
+}

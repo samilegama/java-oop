f4f2fe826694d4e3feb93a4a2c970c68cc5784c1
samilegama <samilegama@gmail.com>
2019-01-13 18:58:18 -0500

    add ifsatement

---
+++ src/ifstatement/Apples.java
@@ -0,0 +1,18 @@
+package ifstatement;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		int a, b;
+		a = 5;
+		b = 15;
+		
+		if (a < 10 && 10 > b) {
+			System.out.println("Maths is true.");
+		}
+		else {
+			System.out.println("I don't trust maths.");
+			}
+		}
+		
+	}

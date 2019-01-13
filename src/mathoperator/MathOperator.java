c139adc83801f2108c15377ef2dbe99d30364854
samilegama <samilegama@gmail.com>
2019-01-13 11:54:34 -0500

    add mathsoperator

---
+++ src/mathoperator/MathOperator.java
@@ -0,0 +1,19 @@
+package mathoperator;
+
+import java.util.Scanner;
+
+public class MathOperator {
+
+	public static void main(String[] args) {
+		Scanner bucky = new Scanner (System.in);
+		int girls, boys, people;
+		girls = 6;
+		boys = 4;
+		people = girls + boys;
+		System.out.println("The number of people is " +people);
+
+
+
+	}
+
+}

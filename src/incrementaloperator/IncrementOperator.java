367c8937ec12bb29f166f1a8ef60034e4f64595f
samilegama <samilegama@gmail.com>
2019-01-13 12:11:29 -0500

    add increment operator

---
+++ src/incrementaloperator/IncrementOperator.java
@@ -0,0 +1,19 @@
+package incrementaloperator;
+
+public class IncrementOperator {
+
+	public static void main(String[] args) {
+		
+		int tuna = 5;
+		int bass = 18;
+		
+		
+		System.out.println(tuna);
+		System.out.println(++tuna);
+		System.out.println(tuna++);
+		System.out.println(tuna);
+		System.out.println(tuna);
+
+	}
+
+}
